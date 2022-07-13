7f16e08dede7c89111aaec17bf3b05427e7b4b8f
hurricane1988 <hurricane_1988@163.com>
2022-07-13 21:00:33 +0800

    add terminal.go

--- service/terminal.go
+++ service/terminal.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/wonderivan/logger"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/remotecommand"
 	"log"
@@ -73,6 +75,44 @@ func (t *terminal) WsHandler(w http.ResponseWriter, r *http.Request) {
 		logger.Info("关闭容器 " + containerName + "的pty终端")
 		pty.Close()
 	}()
+	// 初始化pod所在的corev1资源组
+	// PodExecOptions struct 包括Container stdout stdout  Command 等结构
+	// scheme.ParameterCodec 应该是pod 的GVK （GroupVersion & Kind）之类的
+	// URL长相:
+	// https://192.168.1.11:6443/api/v1/namespaces/default/pods/nginx-wf2-778d88d7c-7rmsk/exec?command=%2Fbin%2Fbash&container=nginx-wf2&stderr=true&stdin=true&stdout=true&tty=true
+	req := K8s.ClientSet.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(namespace).SubResource("exec").VersionedParams(&v1.PodExecOptions{
+		Container: containerName,
+		Command:   []string{"/bin/bash"},
+		Stderr:    true,
+		Stdin:     true,
+		Stdout:    true,
+	}, scheme.ParameterCodec)
+	fmt.Println(req.URL())
+
+	// remotecommand 主要实现了http 转 SPDY 添加X-Stream-Protocol-Version相关header 并发送请求
+	executor, err := remotecommand.NewSPDYExecutor(conf, "POST", req.URL())
+	if err != nil {
+		return
+	}
+	// 建立链接之后从请求的sream中发送、读取数据
+	err = executor.Stream(remotecommand.StreamOptions{
+		Stdout:            pty,
+		Stdin:             pty,
+		Stderr:            pty,
+		TerminalSizeQueue: pty,
+		Tty:               true,
+	})
+	if err != nil {
+		msg := fmt.Sprintf("Exec to pod error! err : %v", err)
+		logger.Info(msg)
+		// 将报错返回出去
+		pty.Write([]byte(msg))
+		// 标记退出stream流
+		pty.Done()
+	}
 }
 
 // NewTerminalSession 该方法用于升级http协议至websocket，并new一个TerminalSession类型的对象返回
@@ -109,13 +149,13 @@ func (t *TerminalSession) Read(p []byte) (int, error) {
 	_, message, err := t.wsConn.ReadMessage()
 	if err != nil {
 		log.Printf("read message err: %v", err)
-		return copy(p, END_OF_TRANSMISSION), err
+		return copy(p, config.END_OF_TRANSMISSION), err
 	}
 	var msg TerminalMessage
 	if err := json.Unmarshal([]byte(message), &msg); err != nil {
 		logger.Error(errors.New("读取parse信息失败,错误信息," + err.Error()))
 		// return 0, nil
-		return copy(p, END_OF_TRANSMISSION), err
+		return copy(p, config.END_OF_TRANSMISSION), err
 	}
 	switch msg.Operation {
 	case "stdin":
